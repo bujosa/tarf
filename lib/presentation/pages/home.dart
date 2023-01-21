adab29b53a1ff684a793e8e763f171178d6e64dc
bujosa <davidbujosa@gmail.com>
2023-01-21 05:31:02 -0400

    feat: add images and gradient in title

--- lib/presentation/pages/home.dart
+++ lib/presentation/pages/home.dart
@@ -6,17 +6,62 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-          title: const Text(
-        'Word of the Day',
-      )),
+        bottom: const PreferredSize(
+            preferredSize: Size.fromHeight(20),
+            child: SizedBox(
+              height: 20,
+            )),
+        centerTitle: true,
+        title: const Text(
+          'Word of the Day',
+          style: TextStyle(fontSize: 40),
+        ),
+        backgroundColor: Colors.transparent,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(15),
+            ),
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: <Color>[
+                Color.fromARGB(255, 88, 174, 244),
+                Color.fromARGB(255, 26, 122, 200)
+              ],
+            ),
+          ),
+        ),
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'Classmate',
-              style: TextStyle(fontSize: 40),
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            // Use Image from web (network)
+            Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              height: MediaQuery.of(context).size.height * 0.4,
+              margin: const EdgeInsets.symmetric(vertical: 20),
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black,
+                    blurRadius: 10,
+                    spreadRadius: 5,
+                  ),
+                ],
+              ),
+              child: Image.network(
+                'https://images.pexels.com/photos/2245436/pexels-photo-2245436.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+            const Text(
+              'Travel',
+              style: TextStyle(fontSize: 40, color: Colors.black),
             ),
           ],
         ),
