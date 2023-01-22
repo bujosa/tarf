db01291aa2f52e3637706f70f69654565f0cfe51
bujosa <davidbujosa@gmail.com>
2023-01-22 18:32:22 -0400

    feat(widgets): add info icons

--- lib/presentation/widgets/bottom_navigation_bar.dart
+++ lib/presentation/widgets/bottom_navigation_bar.dart
@@ -27,8 +27,29 @@ class BottomNavigationBarWidget extends StatelessWidget {
                 width: 36,
                 height: 36,
                 child: IconButton(
-                  icon: const Icon(Icons.home, color: Colors.white),
-                  onPressed: () {},
+                  icon: const Icon(Icons.copy, color: Colors.white),
+                  onPressed: () {
+                    const data = ClipboardData(text: 'Comida');
+
+                    Clipboard.setData(data);
+
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: const Text(
+                        'Reference copied!',
+                        style: TextStyle(
+                            fontSize: 17,
+                            color: Colors.white,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.w400),
+                      ),
+                      behavior: SnackBarBehavior.floating,
+                      backgroundColor: Colors.green,
+                      duration: const Duration(seconds: 2),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ));
+                  },
                 ),
               ),
               GestureDetector(
@@ -68,28 +89,29 @@ class BottomNavigationBarWidget extends StatelessWidget {
                 width: 36,
                 height: 36,
                 child: IconButton(
-                  icon: const Icon(Icons.copy, color: Colors.white),
+                  icon: const Icon(Icons.info_outline,
+                      color: Colors.white, size: 27),
                   onPressed: () {
-                    const data = ClipboardData(text: 'Comida');
-
-                    Clipboard.setData(data);
-
-                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                      content: const Text(
-                        'Reference copied!',
-                        style: TextStyle(
-                            fontSize: 17,
-                            color: Colors.white,
-                            fontFamily: 'Montserrat',
-                            fontWeight: FontWeight.w400),
-                      ),
-                      behavior: SnackBarBehavior.floating,
-                      backgroundColor: Colors.green,
-                      duration: const Duration(seconds: 2),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24),
-                      ),
-                    ));
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text('About',
+                                style: TextStyle(color: Colors.white)),
+                            backgroundColor: const Color(0xFF17203A),
+                            content: const Text(
+                                'This is a word of the day app made using a free APIs from dictionaryapi.dev',
+                                style: TextStyle(color: Colors.white)),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('OK'),
+                              )
+                            ],
+                          );
+                        });
                   },
                 ),
               ),
